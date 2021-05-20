9616ca8658d1d76e2a7c62e4b7e0e684c0cca56f
dtid_4cb73ba1d0cd43af <396751927@qq.com>
2021-05-20 15:17:46 +0800

    init

--- src/Service.php
+++ src/Service.php
@@ -8,10 +8,6 @@ use yzh52521\Aliyun\command\Publish;
 class Service extends \think\Service
 {
 
-    public function register()
-    {
-        // $this->app->bind('AliyunVod', new AliyunVod(config('think-aliyun-vod.aliyunvod.default')));
-    }
     public function boot()
     {
         $this->commands(['aliyunvod:publish' => Publish::class]);
